5b42ef00dba962e873f820243687e8ca4f9d45ba
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-03-25 22:43:59 +0100

    added nginx git

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,6 +1,8 @@
 mkdir inputFiles
 mkdir outputFiles
 mkdir outputFiles/styles
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-backend/develop/conf/config.yml
+curl -O https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-backend/develop/conf/config.yml 
 mkdir frontend
 curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/develop | tar xz --strip=1   -C frontend
+curl -O https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf
+mv conf frontend
