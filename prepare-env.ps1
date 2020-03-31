743d89c285f9f2f31f19557d6d0f1be04c25e1b9
eliasborngaesser <60789000+eliasborngaesser@users.noreply.github.com>
2020-03-31 19:01:37 +0200

    working prepare-env.ps1

--- prepare-env.ps1
+++ prepare-env.ps1
@@ -1,10 +1,10 @@
-$folder=netcdf-leaflet
-mkdir $folder
+$folder='netcdf-leaflet'
+mkdir "$folder"
 curl https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/master -O ./$folder/frontend.tar
-mkdir ./$folder/frontend
-tar -xvf ./$folder/frontend.tar -C "./$folder/frontend"
-Remove-Item frontend.tar
-mv ./$folder/frontend/*/** ./frontend
-Remove-Item ./$folder/frontend/eliasborngaesser-* -Recurse
-curl https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml -O config.yml
-curl https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf -O nginx-conf
+mkdir "./$folder/frontend"
+tar -xvf "./$folder/frontend.tar" -C "./$folder/frontend"
+Remove-Item "./$folder/frontend.tar"
+mv "./$folder/frontend/*/**" "./$folder/frontend"
+Remove-Item "./$folder/frontend/eliasborngaesser-*" -Recurse
+curl https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml -O "./$folder/config.yml"
+curl https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf -O "./$folder/nginx-conf"
