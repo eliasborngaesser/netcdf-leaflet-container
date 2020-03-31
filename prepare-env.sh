64e928c9f9825f20626b00a3b6101a9482ea6a5e
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-03-31 13:44:35 +0200

    prepare-env.sh now unnecesarry

--- prepare-env.sh
+++
@@ -1,6 +0,0 @@
-mkdir inputFiles
-mkdir outputFiles
-mkdir outputFiles/styles
-#curl -O https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-backend/develop/conf/config.yml 
-mkdir frontend
-curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/develop | tar xz --strip=1   -C frontend
