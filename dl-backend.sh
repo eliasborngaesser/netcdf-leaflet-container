8824ee866914f6f20b507e152454efd441eb4d74
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-03-31 13:43:50 +0200

    now using master

--- dl-backend.sh
+++ dl-backend.sh
@@ -1,2 +1,2 @@
 mkdir backend
-curl -L https://github.com/eliasborngaesser/netcdf-leaflet-backend/tarball/develop | tar xz --strip=1   -C backend
+curl -L https://github.com/eliasborngaesser/netcdf-leaflet-backend/tarball/master | tar xz --strip=1   -C backend
