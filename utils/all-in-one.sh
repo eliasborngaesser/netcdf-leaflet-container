a5bf9267b1bf5c0c997cb1c627f0fadf78e37ab6
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-04-04 01:27:40 +0200

    update

--- utils/all-in-one.sh
+++ utils/all-in-one.sh
@@ -1,8 +1,8 @@
-./silent-build.sh
+./utils/silent-build.sh
 docker rm geoserver -f
 docker run --rm -d -p 8600:8080 --name geoserver -e STABLE_EXTENSIONS=netcdf-plugin kartoza/geoserver:2.16.2
- ./dl-frontend.sh
-docker run --rm --mount type=bind,source="$(pwd)",target=/work -t netcdf-leaflet-container:latest -c minimal-config.yml -i inputFiles/MZ_Flow_2018-07-22_05.00.00-new.nc -p test
+ ./prepare-env.sh
+docker run -it --rm --mount type=bind,source="$(pwd)/netcdf-leaflet",target=/work -t eliasborngaesser/netcdf-leaflet-container:latest -p test create
 docker rm nginx -f
-docker run --rm -d -p 8081:80 --name nginx -v $PWD/frontend:/usr/share/nginx/html -v $PWD/nginx/conf:/etc/nginx/conf -t nginx:latest 
-python -mwebbrowser http://localhost:8081/app/projects/test/index.html
+docker run --rm -d -p 8081:80 --name nginx -v $(pwd)/netcdf-leaflet/frontend:/usr/share/nginx/html -v $PWD/netcdf-leaflet/nginx-conf:/etc/nginx/conf -t nginx:latest
+python -mwebbrowser http://localhost:8081/app/projects/test1/index.html
