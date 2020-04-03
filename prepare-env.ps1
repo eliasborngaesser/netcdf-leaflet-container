1ac0f82533bdd4a5efd5f9a2986c4bd6c5fef2ad
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-04-04 01:24:46 +0200

    feat(env) now using static inputFolder

--- prepare-env.ps1
+++ prepare-env.ps1
@@ -2,6 +2,8 @@ $folder='netcdf-leaflet'
 mkdir "$folder"
 curl https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/master -O ./$folder/frontend.tar
 mkdir "./$folder/frontend"
+mkdir "./$folder/inputFiles"
+echo "Place yout input .nc Files in this Folder" > $folder/inputFiles/README.txt
 tar -xvf "./$folder/frontend.tar" -C "./$folder/frontend"
 Remove-Item "./$folder/frontend.tar"
 mv "./$folder/frontend/*/**" "./$folder/frontend"
--- prepare-env.sh
+++ prepare-env.sh
@@ -1,5 +1,7 @@
 folder=netcdf-leaflet
-mkdir $folder 
+mkdir $folder
+mkdir $folder/inputFiles
+echo "Place yout input .nc Files in this Folder" > $folder/inputFiles/README.txt
 ./utils/dl-frontend.sh
 mv frontend $folder
 curl -L  -o "$folder/config.yml" 'https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml'
