9e5c476b906b9a2e920eb5a53420866c72093b46
eliasborngaesser <60789000+eliasborngaesser@users.noreply.github.com>
2020-04-04 03:16:19 +0200

    feat(prepare-env) now .bat compatible

---
+++ prepare-env.bat
@@ -0,0 +1,11 @@
+set folder=netcdf-leaflet
+mkdir %folder% %folder%-tmp
+curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/master -o .\%folder%-tmp\frontend.tar
+tar -xvf ".\%folder%-tmp\frontend.tar" -C "%folder%-tmp"
+mkdir "%folder%\frontend"
+xcopy "%folder%-tmp\*" ".\%folder%\frontend"
+rd /Q /S "%folder%-tmp"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml -o ".\%folder%\config.yml"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf -o ".\%folder%\nginx-conf"
+mkdir "%folder%\inputFiles"
+echo Place your input .nc Files in this directory > "%folder%\inputFiles\README.txt"
