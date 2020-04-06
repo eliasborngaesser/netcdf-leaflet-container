e04cd1ea2b21a6ee9294731cecf2f3ad2da220ce
eliasborngaesser <60789000+eliasborngaesser@users.noreply.github.com>
2020-04-06 19:12:26 +0200

    now finally working

--- prepare-env.bat
+++ prepare-env.bat
@@ -1,12 +1,16 @@
 set folder=netcdf-leaflet
 mkdir %folder% %folder%-tmp
-curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/master -o .\%folder%-tmp\frontend.tar
-tar -xvf ".\%folder%-tmp\frontend.tar" -C "%folder%-tmp"
+curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/master -o %folder%-tmp\frontend.tar
+tar -xvf "%folder%-tmp\frontend.tar" -C "%folder%-tmp"
 mkdir "%folder%\frontend"
-xcopy "%folder%-tmp\*" ".\%folder%\frontend"
+
+cd "%folder%-tmp/eliasborngaesser-*"
+xcopy /S "app" "..\..\%folder%\frontend\app\"
+cd "../../"
 rd /Q /S "%folder%-tmp"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml -o ".\%folder%\config.yml"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf -o ".\%folder%\nginx-conf"
+
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml -o "%folder%\config.yml"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf -o "%folder%\nginx-conf"
 mkdir "%folder%\inputFiles"
 echo Place your input .nc Files in this directory > "%folder%\inputFiles\README.txt"
 pause
