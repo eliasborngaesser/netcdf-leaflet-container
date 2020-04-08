886fa74d25fc6a95ea9810a2de5378a9986efb05
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-04-08 13:14:58 +0200

    feat(silent-build) now ability to define version

--- utils/silent-build.sh
+++ utils/silent-build.sh
@@ -1,7 +1,11 @@
+if [ ! $1 ]; then
+    echo "Usage: $0 <IMAGEVERSION>"
+    exit 1
+fi
 mkdir build 
 ./utils/dl-backend.sh
 mv backend build
 cp Dockerfile build
 docker rmi $(docker images -f "dangling=true" -q) -f
-docker build --rm -t eliasborngaesser/netcdf-leaflet-container:latest ./build
+docker build --rm -t eliasborngaesser/netcdf-leaflet-container:$1 ./build
 rm -R build
