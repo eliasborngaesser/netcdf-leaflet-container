7584bf9b0190c898bd546710ee3a79c49214f130
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-04-14 22:09:23 +0200

    fix(silent-build)

--- utils/silent-build.sh
+++ utils/silent-build.sh
@@ -2,10 +2,13 @@ if [ ! $1 ]; then
     echo "Usage: $0 <IMAGEVERSION>"
     exit 1
 fi
-mkdir build 
+mkdir build
 ./utils/dl-backend.sh
 mv backend build
 cp Dockerfile build
 docker rmi $(docker images -f "dangling=true" -q) -f
 docker build --rm -t eliasborngaesser/netcdf-leaflet-container:$1 ./build
 rm -R build
+if [ $2 = 'push' ]; then
+    docker push eliasborngaesser/netcdf-leaflet-container:$1
+fi
