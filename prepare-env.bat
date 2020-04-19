699220138f64dbea736e204ea1303314d4d5eb48
eliasborngaesser <60789000+eliasborngaesser@users.noreply.github.com>
2020-04-19 20:12:31 +0200

    Update prepare-env.bat

--- prepare-env.bat
+++ prepare-env.bat
@@ -11,9 +11,9 @@ cd "../../"
 rd /Q /S "%folder%-tmp"
 
 curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/minimal-config.yml -o "%folder%\config.yml"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/nginx/conf -o "%folder%\conf\nginx-conf"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/geoserver/netcdf.projections.properties -o "%folder%\netcdf.projections.properties"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/geoserver/content.ftl -o "%folder%\content.ftl"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/nginx/conf -o "%folder%\conf\nginx\conf"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/geoserver/netcdf.projections.properties -o "%folder%\conf\geoserver\netcdf.projections.properties"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/geoserver/content.ftl -o "%folder%\conf\geoserver\content.ftl"
 mkdir "%folder%\inputFiles"
 echo Place your input .nc Files in this directory > "%folder%\inputFiles\README.txt"
 pause
