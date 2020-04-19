2a9a59a35ad360edc73a86f3b72fd003a8b82b7b
EliasBorngaesser <elias.borngaesser@gmail.com>
2020-04-19 16:17:13 +0200

    feat(usability) added branch-name as variable

--- prepare-env.bat
+++ prepare-env.bat
@@ -1,17 +1,19 @@
 set folder=netcdf-leaflet
+set branch=develop
 mkdir %folder% %folder%-tmp
-curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/master -o %folder%-tmp\frontend.tar
+curl -L https://github.com/eliasborngaesser/netcdf-leaflet-frontend/tarball/%branch% -o %folder%-tmp\frontend.tar
 tar -xvf "%folder%-tmp\frontend.tar" -C "%folder%-tmp"
-mkdir "%folder%\frontend"
+mkdir "%folder%\frontend" "%folder%\conf" "%folder%\conf\geoserver" "%folder%\conf\nginx"
 
 cd "%folder%-tmp/eliasborngaesser-*"
 xcopy /S "app" "..\..\%folder%\frontend\app\"
 cd "../../"
 rd /Q /S "%folder%-tmp"
 
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml -o "%folder%\config.yml"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf -o "%folder%\nginx-conf"
-curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/netcdf.projections.properties -o "netcdf.projections.properties"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/minimal-config.yml -o "%folder%\config.yml"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/nginx/conf -o "%folder%\conf\nginx-conf"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/geoserver/netcdf.projections.properties -o "%folder%\netcdf.projections.properties"
+curl -L https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/%branch%/conf/geoserver/content.ftl -o "%folder%\content.ftl"
 mkdir "%folder%\inputFiles"
 echo Place your input .nc Files in this directory > "%folder%\inputFiles\README.txt"
 pause
--- prepare-env.sh
+++ prepare-env.sh
@@ -1,9 +1,10 @@
 folder=netcdf-leaflet
-mkdir $folder
-mkdir $folder/inputFiles
+branch=develop
+mkdir $folder $folder/inputFiles $folder/conf $folder/conf/geoserver $folder/conf/nginx
 echo "Place yout input .nc Files in this Folder" > $folder/inputFiles/README.txt
-./utils/dl-frontend.sh
+./utils/dl-frontend.sh $branch
 mv frontend $folder
-curl -L  -o "$folder/config.yml" 'https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/minimal-config.yml'
-curl -L  -o "$folder/nginx-conf" 'https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/nginx/conf'
-curl -L  -o "netcdf.projections.properties" 'https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/master/netcdf.projections.properties'
+curl -L  -o "$folder/config.yml" "https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/$branch/conf/minimal-config.yml"
+curl -L  -o "$folder/conf/nginx/conf" "https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/$branch/conf/nginx/conf"
+curl -L  -o "$folder/conf/geoserver/netcdf.projections.properties" "https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/$branch/conf/geoserver/netcdf.projections.properties"
+curl -L  -o "$folder/conf/geoserver/content.ftl" "https://raw.githubusercontent.com/eliasborngaesser/netcdf-leaflet-container/$branch/conf/geoserver/content.ftl"
